7a5af618dafe854c8321ac4d459cdcac4126b304
yongss <bbl737898@gmail.com>
2025-09-17 11:09:58 +0900

    feat: 전략 패턴 적용
    - 전략 패턴 적용
    - 익명 내부 클래스 적용
    - 전략을 파라미터로 전달 받기

--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,9 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.template.code.AbstractTemplate;
 import hello.advanced.trace.template.code.SubClassLogic1;
 import hello.advanced.trace.template.code.SubClassLogic2;
@@ -38,35 +42,70 @@ public class ContextV1Test {
     }
 
     /**
-     * 템플릿 메서드 패턴 적용
+     * 전략 패턴 사용
      */
     @Test
-    void templateMethodV1() {
-        AbstractTemplate template1 = new SubClassLogic1();
-        template1.execute();
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
 
-        AbstractTemplate template2 = new SubClassLogic2();
-        template2.execute();
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
     }
 
     @Test
-    void templateMethodV2() {
-        AbstractTemplate template1 = new AbstractTemplate() {
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
             @Override
-            protected void call() {
+            public void call() {
                 log.info("비즈니스 로직1 실행");
             }
         };
-        log.info("클래스 이름1={}", template1.getClass());
-        template1.execute();
 
-        AbstractTemplate template2 = new AbstractTemplate() {
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
             @Override
-            protected void call() {
+            public void call() {
                 log.info("비즈니스 로직2 실행");
             }
         };
-        log.info("클래스 이름2={}", template2.getClass());
-        template2.execute();
+
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV4() {
+
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        contextV2.execute();
     }
 }
