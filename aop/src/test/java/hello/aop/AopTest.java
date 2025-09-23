bc46ffe4fb5c61c71d576e42bea362c3b58730c6
yongss <bbl737898@gmail.com>
2025-09-23 17:41:54 +0900

    feat: 포인트컷 모듈화
    - 공통으로 빼기

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -4,6 +4,7 @@ import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -17,7 +18,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
 
     @Autowired
